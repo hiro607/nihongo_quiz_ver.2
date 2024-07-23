f9dee6b79e6fc7c3f73badaa5f1c4b11360c6253
hiro607 <fumihiro.kawashima@gmail.com>
2024-07-23 09:16:33 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,36 @@
 import streamlit as st
 
-st.title('Nihongo Kotoba quiz 100')
+st.markdown("""
+<h1 style="font-family: 'Segoe UI; font-size: 42px; text-align: center;">
+    Nihongo Kotoba Flash cards 250
+</h1>
+""", unsafe_allow_html=True)
 
-st.caption("Cette app a pour but d'apprendre 100 vocabulaires japonais utilisés dans la vie quotidienne")
+st.markdown(f"""
+    <p style='
+            font-family:Segoe UI; 
+            font-size: 18px;
+            text-align: center;
+            margin-top:30px;'>
+            Cette série de flash cards comprend 250 mots regroupés par thème pour vous aider à pratiquer. 
+            Commencez par les thèmes que vous préférez et augmentez ainsi votre vocabulaire. 
+            Les mots que vous ne connaissez pas seront listés à la fin de chaque session de pratique, alors assurez-vous de les copier dans votre carnet et de bien les mémoriser.
+    </p>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    <p style= '
+            text-align: center; 
+            font-family:Segoe UI; 
+            font-style: italic; 
+            font-size: 23px; 
+            background-color:#CEC5F0;
+            border: 2px solid #A5A0CF; 
+            margin-top:30px;
+            margin-bottom:30px;
+            padding:30px 0px; '>
+            «Une langue vous place dans un couloir pour la vie.<br>Deux langues ouvrent toutes les portes en chemin.»<br>‒Frank Smith
+    </p>
+""", unsafe_allow_html=True)
+
+st.image(r"C:\Users\Fumihiro\My_app\nihongo_quiz_ver.2\data\vocabulary_cards.jpg", use_column_width=True)
